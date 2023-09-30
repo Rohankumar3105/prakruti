c18806634fda2356da1d35a425f2a76a86dba869
Rohan Kumar <130719137+Rohankumar3105@users.noreply.github.com>
2023-09-30 22:37:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# pip install git+https://github.com/huggingface/transformers.git
-import transformers
+!pip install transformers
+
 from transformers import pipeline
 import pandas as pd
 qa_pipeline = pipeline("question-answering", model="bert-large-uncased-whole-word-masking-finetuned-squad")
