6f4620f5c7f7ef8c79e27267ec5ea847941487c9
Rohan Kumar <130719137+Rohankumar3105@users.noreply.github.com>
2023-09-30 22:19:08 +0530

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -1,10 +1,8 @@
 import streamlit as st
+import main
 st.title("Health Suggestions")
-query = st.text_input("Enter your name:")
-received_data = None
-def receive_data(data):
-    global received_data
-    received_data = data
+query = st.text_input("Enter your Query:")
+received_data = main.question(query)
 if st.button("Process"):
     # Call the Python file to process the query
     st.write("Processed Result:")
